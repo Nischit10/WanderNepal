a0afd34d561dbf60c490f1904ad852c9e4af1f1c
Aagman Karki <kaagman8778@gmail.com>
2026-05-07 22:11:39 +0545

    US-11: GET /api/destinations/:id - single destination detail

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import SignupView, SigninView, DestinationListView
+from .views import SignupView, SigninView, DestinationListView, DestinationDetailView
 
 urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
     path('signin/', SigninView.as_view(), name='signin'),
     path('destinations/', DestinationListView.as_view(), name='destinations'),
+    path('destinations/<int:pk>/', DestinationDetailView.as_view(), name='destination-detail'),
 ]
--- backend/api/views/destination.py
+++ backend/api/views/destination.py
@@ -5,3 +5,7 @@ from api.serializers import DestinationSerializer
 class DestinationListView(generics.ListAPIView):
     queryset = Destination.objects.all()
     serializer_class = DestinationSerializer
+
+class DestinationDetailView(generics.RetrieveAPIView):
+    queryset = Destination.objects.all()
+    serializer_class = DestinationSerializer
